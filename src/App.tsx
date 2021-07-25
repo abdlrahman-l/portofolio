780fc692ec3ec6861e340a27b4616034d25ea6f0
rahmanister <10ipa3.abdulrahman@gmail.com>
2021-07-26 02:50:53 +0700

    cmt to repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import AnimationRevealPage from './helpers/AnimationRevealPage';
+import Hero from "./components/hero/TwoColumnWithPrimaryBackground"
+import ThreeColContactDetails from './components/cards/ThreeColContactDetails';
 
+const description = 'I was developing / creating website application using React, php (CodeIgniter) and mobile application using React Native and Android Native. I like to solve problem through code, data structure and algorithm'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AnimationRevealPage>
+        <Hero heading={"Hi.. I'm Abdul Rahman and I'm Software Engineer"}
+          description={description} />
+      </AnimationRevealPage>
+      <AnimationRevealPage>
+        <ThreeColContactDetails />
+      </AnimationRevealPage>
+    </>
   );
 }
 
